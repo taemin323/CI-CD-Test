07829ad519cb35f73fec88e1d2e96c11f2da94cd
taemin <ktm990323@naver.com>
2025-02-27 14:54:17 +0900

    CI fail test

--- src/test/java/com/example/ci_cd_test/CiCdTestApplicationTests.java
+++ src/test/java/com/example/ci_cd_test/CiCdTestApplicationTests.java
@@ -2,12 +2,15 @@ package com.example.ci_cd_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CiCdTestApplicationTests {
 
     @Test
     void contextLoads() {
+        //의도적으로 실패하는 어서션 추가
+        assertEquals(true, false, "의도적으로 실패하는 테스트입니다.");
     }
 
 }
